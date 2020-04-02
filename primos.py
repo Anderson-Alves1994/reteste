00618b2ed77632b56d0af8e2a7653a24d7648dd9
Anderson-Alves1994 <61031977+Anderson-Alves1994@users.noreply.github.com>
2020-04-02 19:11:02 -0300

    reteste

---
+++ primos.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask, jsonify, request
+from math import sqrt
+
+app = Flask(__name__)
+
+@app.route('/')
+def nao_entre_em_panico():
+
+    def primo (n):
+    if n != 0 and n !=1:
+        if n>3:
+            for i in range(2,n):
+                if n %i==0:
+                    return False
+        return True
+    return
+
+
+i=1
+cont =0
+while True:
+    primo(i)
+    i+=1
+    if primo(i) == True:
+        cont+=1
+        print(i)
+    if cont == 100:
+        break
